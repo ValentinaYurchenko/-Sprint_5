816de4f4b7d21de50a6917019040a302db8bfc39
ValentinaYurchenko <v157301@gmail.com>
2024-10-11 15:04:09 +0300

    first commit

---
+++ data.py
@@ -0,0 +1,13 @@
+# Данные пользователя для корректных логинов
+class Data:
+    name = 'Valentina'
+    email = 'Valentina_Yrchenko_11_123@yandex.ru'
+    password = 'Andreeva.12'
+
+# Список URL для перехода на нужные страницы
+class UrlList:
+    page_main_url = "https://stellarburgers.nomoreparties.site/"  # URL главной страницы Конструктор
+    page_login_url = "https://stellarburgers.nomoreparties.site/login"  # URL страницы логин
+    page_registration_url = "https://stellarburgers.nomoreparties.site/register" # URL страницы регистрации
+
+
